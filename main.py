2415091a4779a0c9d8b3bb67707f4278787e13e6
Shivam-kumar-30012 <your-email@example.com>
2026-07-21 01:14:56 +0500

    Stage 2: read endpoints with 404

--- main.py
+++ main.py
@@ -1,14 +1,41 @@
-from fastapi import FastAPI 
-from pydantic import BaseModel
-
+from fastapi import FastAPI, HTTPException
 
 app = FastAPI()
 
 
-@app.get("/") # just for the homepage
+tasks = [
+    {
+        "id" : 1,
+        "title" : "Task 1",
+        "done" : False
+    },
+    {
+        "id" : 2,
+        "title" : "Task 2",
+        "done" : False
+    },
+    {
+        "id" : 3,
+        "title" : "Task 3",
+        "done" : False
+    }
+]
+
+@app.get("/")
 def read_root():
-    return { "name": "Task API", "version": "1.0", "endpoints": ["/tasks"] }    # when you run the page you will see this message on the homepage
+    return { "name": "Task API", "version": "1.1", "endpoints": ["/tasks"] }
 
 @app.get("/health")
-def health_check():
+def read_health():
     return {"status": "ok"}
+
+@app.get("/tasks")
+def get_task():
+    return tasks
+
+@app.get("/tasks/{task_id}")
+def get_task_by_id(task_id : int):
+    for task in tasks:
+        if task["id"] == task_id: 
+            return task
+    raise HTTPException(status_code = 404, detail = f"Task {task_id} not found")
