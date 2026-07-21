ed71f061103fcd7baf632ff3d023dffd75e62151
Shivam-kumar-30012 <your-email@example.com>
2026-07-22 02:52:26 +0500

    Stage 4: Full CRUD API

--- main.py
+++ main.py
@@ -5,6 +5,9 @@ app = FastAPI()
 
 class CreateTask(BaseModel):
     title : str
+class UpdateTask(BaseModel):
+    title : str | None = None 
+    done  : bool | None = None
 
 tasks = [
     {
@@ -57,3 +60,29 @@ def create_task(create_title : CreateTask):
     newtask = {"id"  : next_id, "title" : create_title.title, "done" : False}
     tasks.append(newtask)
     return newtask
+@app.put("/tasks/{task_id}")
+def update_task(task_id : int, details : UpdateTask):
+    if details.title is not None and not details.title.strip():
+        raise HTTPException(status_code = 400, detail = "Title cannot be empty" )
+    curr_task = None
+    for task in tasks:
+        if(task["id"] == task_id):
+            curr_task = task
+            break
+    if curr_task is None : 
+        raise HTTPException(status_code = 404 , detail = "Unknown ID")
+    if details.title is not None:
+        curr_task["title"] = details.title
+    if details.done is not None:
+        curr_task["done"] = details.done
+    return curr_task
+@app.delete("/tasks/{task_id}", status_code = 204)
+def delete_task(task_id : int):
+    curr_task = None
+    for task in tasks:
+        if(task["id"] == task_id):
+            curr_task = task
+            break
+    if curr_task is None : 
+        raise HTTPException(status_code = 404 , detail = "Unknown ID")
+    tasks.remove(curr_task)
